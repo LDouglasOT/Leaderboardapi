b7eea670c501eaa07a3921fd4ca0601e038cd932
Luzinda Douglas <douglas.luzinda.ot@gmail.com>
2022-11-25 11:18:55 +0300

    setup leadrboard layout

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
   entry: './src/index.js',
@@ -6,14 +7,19 @@ module.exports = {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist'),
   },
+  mode:'development',
+  devServer:{
+      static:'./dist'
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template:"./src/index.html"
+    }),
+  ],
   output: {
     filename: '[name].bundle.js',
     path: path.resolve(__dirname, 'dist'),
     clean: true,
-  },
-  mode:'development',
-  devServer:{
-      static:'./dist'
   },
   module: {
     rules: [
